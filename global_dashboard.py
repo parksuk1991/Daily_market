7429175281b4ddcc697cf71f0aba6530ae4bcc26
parksuk1991 <148017439+parksuk1991@users.noreply.github.com>
2025-07-22 13:53:36 +0900

    Update global_dashboard.py

--- global_dashboard.py
+++ global_dashboard.py
@@ -24,8 +24,9 @@ st.set_page_config(
 # -------------------- 상단 레이아웃: 제목+설명 / 이미지+크레딧 ---------------------
 col_title, col_img_credit = st.columns([7, 1])
 with col_title:
-    st.title("🌐 글로벌 시장 대시보드")
-    st.markdown("####    주요 시장 성과", unsafe_allow_html=True)
+    st.title("🌐 글로벌 주요 시장 모니터링")
+    st.markdown("---", unsafe_allow_html=True)
+    #st.markdown("####    주요 시장 성과", unsafe_allow_html=True)
 with col_img_credit:
     # 닐 암스트롱 달착륙 사진(퍼블릭 도메인, NASA) - 다운로드 실패시 대체 아이콘 제공
     image_url = "https://cdn.theatlantic.com/thumbor/gjwD-uCiv0sHowRxQrQgL9b3Shk=/900x638/media/img/photo/2019/07/apollo-11-moon-landing-photos-50-ye/a01_40-5903/original.jpg"
