c18fba447893cc72e55055106ea1ccb50fcb6d37
parksuk1991 <148017439+parksuk1991@users.noreply.github.com>
2025-07-22 11:15:51 +0900

    Update global_dashboard.py

--- global_dashboard.py
+++ global_dashboard.py
@@ -35,20 +35,20 @@ with col_img_credit:
         response = requests.get(image_url, timeout=5)
         response.raise_for_status()
         img = Image.open(BytesIO(response.content))
-        st.image(img, width=110, caption=None)
+        st.image(img, width=150, caption=None)
         img_displayed = True
     except Exception:
         try:
             response = requests.get(fallback_icon, timeout=5)
             response.raise_for_status()
             img = Image.open(BytesIO(response.content))
-            st.image(img, width=90, caption=None)
+            st.image(img, width=150, caption=None)
             img_displayed = True
         except Exception:
             st.info("이미지를 불러올 수 없습니다.")
     st.markdown(
-        "<div style='margin-top: -5px; text-align:center;'>"
-        "<span style='font-size:1.0rem; color:#888;'>Made by parksuk1991</span>"
+        "<div style='margin-top: -1px; text-align:center;'>"
+        "<span style='font-size:0.9rem; color:#888;'>Made by parksuk1991</span>"
         "</div>",
         unsafe_allow_html=True
     )
