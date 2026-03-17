7a65213c4c079844f6b3a133a4a67872aad9f336
parksuk1991 <148017439+parksuk1991@users.noreply.github.com>
2026-03-17 17:20:35 +0900

    Update global_dashboard.py

--- global_dashboard.py
+++ global_dashboard.py
@@ -913,7 +913,7 @@ def plot_maximum_drawdown(prices_df, asset_name):
 def show_page1():
     st.markdown(f'<h1 style="color: {TITLE_COLOR};">🌐 Market Performance</h1>', unsafe_allow_html=True)
     update_clicked = st.button("🔄 Update", type="primary", key="p1_update")
-    st.markdown("추후 펀드 내 실제 보유 자산들로 구성하여 개발 예정")
+    st.markdown("*추후 펀드내 실제 보유 자산들로 구성하여 개발 필요")
 
     if update_clicked:
         st.session_state['p1_updated'] = True
