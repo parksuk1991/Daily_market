a10eda2a406d016cfa692bd74d7dd00b09d613d6
CP3 <148017439+parksuk1991@users.noreply.github.com>
2026-07-07 20:44:58 +0900

    Update global_dashboard.py

--- global_dashboard.py
+++ global_dashboard.py
@@ -2888,7 +2888,6 @@ with st.sidebar:
 
 
     st.markdown(f'<h1 style="color: {TITLE_COLOR};">💡 Global Market</h1>', unsafe_allow_html=True)
-    st.caption("*펀드 보유 종목만 분석되도록 개발 예정")
     st.markdown("---")
     page = st.radio(
         "페이지 선택",
